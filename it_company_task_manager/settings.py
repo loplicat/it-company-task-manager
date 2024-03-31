33961d08aa130e09f6230f4aa90a773206634e28
Lana Chernenko <svitlaana.chernenko.s@gmail.com>
2024-03-31 13:43:17 +0100

    Remove SECRET_KEY out from settings.py and place it in .env file

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-e9%zo7r-g48h)=i-9m5&9r4l09+h$si3-pb&muapg64a+ppk81")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
